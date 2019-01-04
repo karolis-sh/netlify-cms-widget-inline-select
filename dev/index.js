73a14258c849aaec233a745b39aa21e445bd8ed9
Karolis Šarapnickis <pastas.k@gmail.com>
2019-01-04 21:53:27 +0200

    chore: update dependencies

--- dev/index.js
+++ dev/index.js
@@ -66,7 +66,7 @@ const config = {
               label: 'Inline radio select with long titles',
               widget: 'inline-select',
               options: [
-                `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
                 'looooooooooooooooooooooooooooooooooooooooooooooooong selection',
               ],
             },
