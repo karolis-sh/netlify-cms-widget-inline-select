c302b7f6369c7b4e8c60591c3d6176a56efda668
Karolis Šarapnickis <pastas.k@gmail.com>
2021-09-23 07:42:28 +0300

    chore: add release entry script

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$(npm whoami)" ]; then
+    echo "🚨 Not logged in to npm"
+    exit 1
+fi
+
+if [ -z "$(git status --porcelain)" ]; then
+    git checkout master
+    git pull
+    yarn release
+else
+    git status --porcelain
+    echo "🔼 Working directory not clean";
+    exit 1
+fi
